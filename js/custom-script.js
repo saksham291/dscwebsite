71cf54af44c47b2cc0316854b5f339d357b08edb
Saksham Bhushan <saksham291@gmail.com>
2019-04-10 05:39:00 +0530

    added contact us and page interlinking

--- js/custom-script.js
+++ js/custom-script.js
@@ -14,3 +14,5 @@ document.addEventListener('DOMContentLoaded', function () {
 	var elems = document.querySelectorAll('.sidenav');
 	var instances = M.Sidenav.init(elems);
 });
+var collapsibleElem = document.querySelector('.collapsible');
+var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
