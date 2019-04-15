6413f49e1bb9a5fba04d692fc88c50100a36e389
ambarytl <ambar.ytl@gmail.com>
2019-04-15 13:54:30 +0530

    fix parallax

--- aboutus.php
+++ aboutus.php
@@ -1,86 +1,88 @@
 <html>
 
 <head>
-    <meta charset="UTF-8">
-    <title>
-        Developer Student Club - IIT Bhilai
-    </title>
+  <meta charset="UTF-8">
+  <title>
+    Developer Student Club - IIT Bhilai
+  </title>
 
 </head>
 
 <body>
-<?php require 'header.php'; ?>
-    <main>
-        <div class="parallax-container">
-            <!-- TODO: Replace with group photo -->
-            <div class="parallax"><img src="https://materializecss.com/images/parallax2.jpg"></div>
-        </div>
+  <?php require 'header.php'; ?>
+  <main>
+    <div class="parallax-container">
+      <!-- TODO: Replace with group photo -->
+      <div class="parallax"><img src="https://materializecss.com/images/parallax2.jpg"></div>
+    </div>
 
-        <div class="container" style="width:90%;">
-            <h4 class="login-form-text center" style="font-family: 'Arvo', serif; font-size:190%;margin-top:2%;margin-bottom:2%;"><small>Indian Institute of Technology Bhilai</small><br>Developer Student Club<br><span style="font-size:74%;">Powered by Google Developers</span></h4>
+    <div class="container" style="width:90%;">
+      <h4 class="login-form-text center" style="font-family: 'Arvo', serif; font-size:190%;margin-top:2%;margin-bottom:2%;"><small>Indian Institute of Technology Bhilai</small><br>Developer Student Club<br><span style="font-size:74%;">Powered by Google Developers</span></h4>
 
-            <p style="margin-left:10%;">Google collaborates with university students who are passionate about growing developer communities and
-                supports them with starting student clubs on their campuses.</p>
-            <p style="margin-left:10%;">These DSCs provide students with the opportunity to:</p>
-            <ul style="margin-left:10%;">
-                <li>Grow their knowledge on developer technologies and more through peer to peer workshops
-                    and events.</li>
-                <li>Gain relevant industry experience by solving problems for local organizations with
-                    technology based solutions.</li>
-                <li>Showcase their prototypes and solutions to their local community and industry
-                    leaders.</li>
-            </ul>
-            <p style="margin-left:10%;">Google Developers supports and recognizes DSCs, but does not own or manage them.</p>
-            <p style="margin-left:10%;">The following are the core members of the Student Developer Club - IIT Bhilai</p>
-          <p style="margin-left:10%;">  <b>DSC Lead</b><br>
-                Arjun Singh Kushwaha<br>
-                <br>
-              <b>Cloud Development</b><br>
-                1. Ahaan Dabolkar<br>
-                2. Rishabh Kumar Poddar<br>
-                3. Rachuri Sri Pramodh<br>
-<br>
-              <b>  Competitive Programming</b><br>
-                1. Devansh Bajpai<br>
-                2. Shubham Agrawal<br>
-                3. Saptarshi Mukherjee<br>
-<br>
-              <b>  Web Development</b><br>
-                1. Saksham Bhushan<br>
-                2. Kumar Shivendu<br>
-                3. Ambar Mutha<br>
-<br>
-              <b>  App Development</b><br>
-                1. Vaibhav Kumar<br>
-                2. Pramit Bhattacharyya<br>
-                3. Vishal Sharma<br>
-<br>
-              <b>  Machine Learning</b><br>
-                1. Anees Ahmed<br>
-                2. Pintu Kumar<br>
-                3. Akash Patel<br>
-                4. Harshvardhan Patel<br>
-                5. Neelmani Gautam<br>
-<br>
-              <b>  Event Management</b><br>
-                1. Akash Singh<br>
-                2. Govind Kumar Arora<br>
-                3. Udit Kushwaha<br>
-                4. Aikata<br>
-                5. Debajyoti Halder<br>
-                6. Siram nikhil n datha sai kumar<br>
-                7. Kumar Shivam<br>
-                8. Mayank Kumar<br>
-<br>
-              <b>  Event Coverage (Photography)</b><br>
-                1. Amit Kumar<br>
-                2. Saksham Bhushan<br>
-<br>
-              <b>  Outreach</b><br>
-                1. Pratik Sanjay Patil<br>
-                2. Hrishikesh Pradip Kaulwar</p><br>
-        </div>
-    </main>
+      <p style="margin-left:10%;">Google collaborates with university students who are passionate about growing developer communities and
+        supports them with starting student clubs on their campuses.</p>
+      <p style="margin-left:10%;">These DSCs provide students with the opportunity to:</p>
+      <ul style="margin-left:10%;">
+        <li>Grow their knowledge on developer technologies and more through peer to peer workshops
+          and events.</li>
+        <li>Gain relevant industry experience by solving problems for local organizations with
+          technology based solutions.</li>
+        <li>Showcase their prototypes and solutions to their local community and industry
+          leaders.</li>
+      </ul>
+      <p style="margin-left:10%;">Google Developers supports and recognizes DSCs, but does not own or manage them.</p>
+      <p style="margin-left:10%;">The following are the core members of the Student Developer Club - IIT Bhilai</p>
+      <p style="margin-left:10%;"> <b>DSC Lead</b><br>
+        Arjun Singh Kushwaha<br>
+        <br>
+        <b>Cloud Development</b><br>
+        1. Ahaan Dabolkar<br>
+        2. Rishabh Kumar Poddar<br>
+        3. Rachuri Sri Pramodh<br>
+        <br>
+        <b> Competitive Programming</b><br>
+        1. Devansh Bajpai<br>
+        2. Shubham Agrawal<br>
+        3. Saptarshi Mukherjee<br>
+        <br>
+        <b> Web Development</b><br>
+        1. Saksham Bhushan<br>
+        2. Kumar Shivendu<br>
+        3. Ambar Mutha<br>
+        <br>
+        <b> App Development</b><br>
+        1. Vaibhav Kumar<br>
+        2. Pramit Bhattacharyya<br>
+        3. Vishal Sharma<br>
+        <br>
+        <b> Machine Learning</b><br>
+        1. Anees Ahmed<br>
+        2. Pintu Kumar<br>
+        3. Akash Patel<br>
+        4. Harshvardhan Patel<br>
+        5. Neelmani Gautam<br>
+        <br>
+        <b> Event Management</b><br>
+        1. Akash Singh<br>
+        2. Govind Kumar Arora<br>
+        3. Udit Kushwaha<br>
+        4. Aikata<br>
+        5. Debajyoti Halder<br>
+        6. Siram nikhil n datha sai kumar<br>
+        7. Kumar Shivam<br>
+        8. Mayank Kumar<br>
+        <br>
+        <b> Event Coverage (Photography)</b><br>
+        1. Amit Kumar<br>
+        2. Saksham Bhushan<br>
+        <br>
+        <b> Outreach</b><br>
+        1. Pratik Sanjay Patil<br>
+        2. Hrishikesh Pradip Kaulwar</p><br>
+    </div>
+  </main>
+  <?php require 'footer.php'; ?>
+  <script src="js/aboutus.js"></script>
 </body>
-<?php require 'footer.php'; ?>
+
 </html>
