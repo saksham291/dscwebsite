1e3ecbf2252b20517005a39d51e0c88466472f16
ambarytl <ambar.ytl@gmail.com>
2019-04-23 22:21:14 +0530

    change internal links to external

--- header.php
+++ header.php
@@ -33,15 +33,15 @@
   <nav>
     <div class="nav-wrapper white">
       <a href="#" data-target="slide-out" class="sidenav-trigger hide-on-large-only left"><i style="padding: 1% 4% 1% 1%;" class="material-icons">menu</i></a>
-      <a href="index.php#top" class="brand-logo grey-text text-darken-1 hide-on-med-and-down" style="margin-top:0.7%;margin-left:1%;font-family: 'Arvo', serif;line-height:113%; font-size:120%"> <img src="new_logo.png" height="35%" alt=""> <br><p style="margin:0% 0% 0% 19%;">IIT Bhilai</p></a>
+      <a href="index.php" class="brand-logo grey-text text-darken-1 hide-on-med-and-down" style="margin-top:0.7%;margin-left:1%;font-family: 'Arvo', serif;line-height:113%; font-size:120%"> <img src="new_logo.png" height="35%" alt=""> <br><p style="margin:0% 0% 0% 19%;">IIT Bhilai</p></a>
       <!-- responsive logo start-->
-      <a href="index.php#top" class="brand-logo grey-text text-darken-1 hide-on-large-only" > <img src="new_logo.png" height="40%" style="margin-top:4.5%;margin-left:5%;" alt=""><span style="font-family: 'Arvo', serif;line-height:90%; font-size:38%;position:relative;margin-top:1%;margin-left:24%"><br>Indian Institute of Technology Bhilai</span></a>
+      <a href="index.php" class="brand-logo grey-text text-darken-1 hide-on-large-only" > <img src="new_logo.png" height="40%" style="margin-top:4.5%;margin-left:5%;" alt=""><span style="font-family: 'Arvo', serif;line-height:90%; font-size:38%;position:relative;margin-top:1%;margin-left:24%"><br>Indian Institute of Technology Bhilai</span></a>
       <!-- responsive logo end-->
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <?php $curr = basename($_SERVER['PHP_SELF']); ?>
-        <li><a href="index.php#top" class="grey-text text-darken-1 active">Home</a></li>
-        <li><a href="index.php#events" class="grey-text text-darken-1">Events</a></li>
-        <li><a href="index.php#projects" class="grey-text text-darken-1">Projects</a></li>
+        <li><a href="index.php" class="grey-text text-darken-1 active">Home</a></li>
+        <li><a href="events.php" class="grey-text text-darken-1">Events</a></li>
+        <li><a href="projects.php" class="grey-text text-darken-1">Projects</a></li>
         <li><a href="aboutus.php" class="grey-text text-darken-1">About us</a></li>
         <li><a href="contactus.php" class="grey-text text-darken-1">Contact us</a></li>
       </ul>
@@ -52,8 +52,8 @@
 <!-- Sidebar -->
 <ul class="sidenav hide-on-large-only" id="slide-out">
 <li><a href="index.php" class="waves-effect">Home</a></li>
-<li><a href="index.php#events" class="waves-effect">Events</a></li>
-<li><a href="index.php#projects" class="waves-effect">Projects</a></li>
+<li><a href="events.php" class="waves-effect">Events</a></li>
+<li><a href="projects.php" class="waves-effect">Projects</a></li>
 <li><a href="aboutus.php" class="waves-effect">About us</a></li>
 <li><a href="contactus.php" class="waves-effect">Contact us</a></li>
 </ul>
