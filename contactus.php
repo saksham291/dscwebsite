cdec005a1332924362e28f5c0ece3f9a888787bd
ambarytl <ambar.ytl@gmail.com>
2019-04-23 16:33:16 +0530

    design and responsive improvements on contact us

--- contactus.php
+++ contactus.php
@@ -14,7 +14,7 @@
     <!-- TODO: Replace with group photo -->
     <div class="parallax"><img src="parallax2.jpg"></div>
   </div>
-  <section id="top" class="" style="height:80%;background-color:#f7f7f7;">
+  <section id="top" class="" style="min-height: 70vh;background-color:#f7f7f7;">
     <div class="center" style="padding-top:4%;">
       <h4 class="login-form-text" style="font-family: 'Arvo', serif; font-size:190%;margin-top:5%;margin-bottom:0%;">Contact Us</h4>
     </div>
@@ -24,14 +24,16 @@
       <form action="https://mail.google.com/mail?view=cm&tf=0&to=saksham291@gmail.com" method="get" enctype="text/plain">
         <div class="container">
           <div class="row">
-            <div class="input-field col s8 offset-s2">
+            <div class="input-field col s12 m8 offset-s0 offset-m2">
+              <i class="material-icons prefix">subject</i>
               <input id="su" type="text" name="su">
               <label for="su">Subject</label>
             </div>
           </div>
           <div class="row">
-            <div class="input-field col s8 offset-s2">
-              <input id="textarea1" name="body" type="text" class="materialize-textarea">
+            <div class="input-field col s12 m8 offset-s0 offset-m2">
+              <i class="material-icons prefix">message</i>
+              <textarea id="textarea1" class="materialize-textarea" name="body"></textarea>
               <label for="textarea1">Message</label>
             </div>
           </div>
