5519019e8add247103c9ae5ca6b183839dd89787
ambarytl <ambar.ytl@gmail.com>
2019-08-10 01:04:55 +0530

    🐛bug fix: sidenav not working move materializejs below footer

--- footer.php
+++ footer.php
@@ -32,8 +32,10 @@
 <!--plugins.js - Some Specific JS codes for Plugin Settings-->
 <!-- <script type="text/javascript" src="js/plugins.min.js"></script> -->
 
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 <!--custom-script.js - Add your own theme custom JS-->
-<!-- <script type="text/javascript" src="js/custom-script.js"></script> -->
+<script type="text/javascript" src="js/custom-script.js"></script>
 
 <style>
     html {
--- header.php
+++ header.php
@@ -19,8 +19,7 @@
 <!-- Compiled and minified CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
-<!-- Compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
 
 <!-- Custome CSS-->
 <link href="css/custom/custom.min.css" type="text/css" rel="stylesheet" media="screen,projection">
