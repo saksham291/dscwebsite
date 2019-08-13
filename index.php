f633ba4007cc5f59f13037e106d1365f5912e644
Kumar Shivendu <shivendu@iitbhilai.ac.in>
2019-08-13 23:47:11 +0530

    Register message change

--- index.php
+++ index.php
@@ -130,11 +130,9 @@
                             href="http://www.google.com/calendar/event?action=TEMPLATE&dates=20190817T053000Z%2F20190817T120000Z&text=Cloud%20Study%20Jam&location=Room%20107%2C%20207%20and%20Computer%20Centre&details=Cloud%20Study%20Jam%20organized%20by%20Developer%20Student%20club%2C%20Indian%20Institute%20of%20Technology%2C%20Bhilai"
                             rel="noreferrer" target="_blank"><i class="material-icons">event</i></a>
                         <div class="card-content">
-                            <p>Cloud Study Jam under Google Cloud Fest in IIT Bhilai Academic Block. Registrations
+                            <p>Cloud Study Jam under Google Cloud Fest in IIT Bhilai. Registrations
                                 Open
-                                for students of
-                                all Raipur colleges
-                                till 14-Aug
+                                till 16 Aug.
                             </p>
                         </div>
                         <div class="card-action">
