bfa5520f177e1d3a2708408ad1f21f69b3f4a2d3
ambarytl <ambar.ytl@gmail.com>
2019-08-14 01:46:02 +0530

    remove comma

--- footer.php
+++ footer.php
@@ -1,33 +1,33 @@
 <footer class="page-footer blue">
-	<div class="container">
-		<div class="row">
-			<div class="col l6 s12">
-				<h5 class="white-text">Developer Student Club - Indian Institute of Technology, Bhilai</h5>
-				<p class="grey-text text-lighten-4">Powered by Google Developers</p>
-			</div>
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">Developer Student Club - Indian Institute of Technology, Bhilai</h5>
+                <p class="grey-text text-lighten-4">Powered by Google Developers</p>
+            </div>
 
-			<div class="col l4 offset-l2 s12">
-				<h5 class="white-text">Links</h5>
-				<ul>
-					<li><a class="grey-text text-lighten-3" href="index.php">Home</a></li>
-					<li><a class="grey-text text-lighten-3" href="index.php#events">Events</a></li>
-					<li><a class="grey-text text-lighten-3" href="aboutus.php">About Us</a></li>
-				</ul>
-			</div>
-		</div>
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3" href="index.php">Home</a></li>
+                    <li><a class="grey-text text-lighten-3" href="index.php#events">Events</a></li>
+                    <li><a class="grey-text text-lighten-3" href="aboutus.php">About Us</a></li>
+                </ul>
+            </div>
+        </div>
 
-		<div class="center-align" style="margin-bottom: 10px;">
-			<a href="https://facebook.com/dsciitbhilai"><i class="fa fa-2x fa-facebook"></i></a>
-			<a href="https://instagram.com/dsc_iitbhilai"><i class="fa fa-2x fa-instagram"></i></a>
-			<a href="https://www.linkedin.com/company/developer-student-club-iit-bhilai/"><i class="fa fa-2x fa-linkedin"></i></a>
-		</div>
-	</div>
+        <div class="center-align" style="margin-bottom: 10px;">
+            <a href="https://facebook.com/dsciitbhilai"><i class="fa fa-2x fa-facebook"></i></a>
+            <a href="https://instagram.com/dsc_iitbhilai"><i class="fa fa-2x fa-instagram"></i></a>
+            <a href="https://www.linkedin.com/company/developer-student-club-iit-bhilai/"><i class="fa fa-2x fa-linkedin"></i></a>
+        </div>
+    </div>
 
-	<div class="footer-copyright">
-		<div class="container">
-			© 2019 Developer Student Club IIT Bhilai
-		</div>
-	</div>
+    <div class="footer-copyright">
+        <div class="container">
+            © 2019 Developer Student Club IIT Bhilai
+        </div>
+    </div>
 </footer>
 <!-- Scripts -->
 
