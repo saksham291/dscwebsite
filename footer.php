90de31fcecb81ccc2fea714562afb733e875dcc7
Kumar Shivendu <shivendu@iitbhilai.ac.in>
2019-08-14 19:32:59 +0530

    Added hotjar for analysis

--- footer.php
+++ footer.php
@@ -35,3 +35,15 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 <!--custom-script-->
 <script type="text/javascript" src="js/custom-script.js"></script>
+
+<!-- Hotjar Tracking Code for dsc.iitbhilai.ac.in -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1444285,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
